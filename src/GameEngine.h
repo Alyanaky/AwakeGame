60e1735b43ee4e9d61207082ec6742a12f407310
Alyanaky <007izosimov@gmail.com>
2025-01-04 15:51:17 +0500

    #

--- src/GameEngine.h
+++ src/GameEngine.h
@@ -4,25 +4,24 @@
 #include "Player.h"
 
 enum class GameState {
-	MENU,
-	PLAYING,
-	PAUSE
+    MENU,
+    PLAYING,
+    PAUSE
 };
 
 class GameEngine {
 public:
-	GameEngine();
-	~GameEngine();
-	bool Initialize();
-	void Run();
-	void Update(float deltaTime);
-	void Render();
+    GameEngine();
+    ~GameEngine();
+    bool Initialize();
+    void Run();
+    void Update(float deltaTime);
+    void Render();
 
 private:
-	Renderer* renderer_;
-	InputManager* inputManager_;
-	Player* player_;
-	GameState gameState_ = GameState::MENU;
-	bool running_ = false;
-
+    Renderer* renderer_;
+    InputManager* inputManager_;
+    Player* player_;
+    GameState gameState_ = GameState::MENU;
+    bool running_ = false;
 };
